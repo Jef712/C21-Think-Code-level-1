db9b5864d1f0dbaa962b07aa63c0e97931741c8b
Jef712 <Jefvdlaarse712@gmail.com>
2021-11-22 11:24:27 +0100

    opdracht5af

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,7 +1,4 @@
-#maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
-#elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
-#weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
-
+# Prints out the name of a favorite game
 def favorite_game(name):
     print("One of my favorite games is", name)
 
